f4d2e7046723f7c29fe1e5d2066d095e26c74efc
권길남 <nimpa3201@naver.com>
2024-12-26 21:23:44 +0900

    Feat: JdbcTemplateItemRepositoryV1 구현
    - JdbcTemplate을 사용하여 ItemRepository 구현
    - save(), update(), findById(), findAll() 메서드 구현
    - 동적 쿼리 처리 로직 추가
    - RowMapper를 활용한 객체 매핑 처리

--- src/main/java/hello/itemservice/repository/jdbctemplate/JdbcTemplateItemRepositoryV1.java
+++ src/main/java/hello/itemservice/repository/jdbctemplate/JdbcTemplateItemRepositoryV1.java
@@ -73,6 +73,7 @@ public class JdbcTemplateItemRepositoryV1 implements ItemRepository {
 
 
 
+
     @Override
     public List<Item> findAll(ItemSearchCond cond) {
         String itemName = cond.getItemName();
